128e7c027ac260faae705cb176b79c741ca94af0
Sebastien Boutin <seb.boutin44@gmail.com>
2022-10-14 20:16:33 -0600

    Replaced raw response with a callback function.

--- index.js
+++ index.js
@@ -2,30 +2,23 @@ const app = require('express')();
 const PORT = 8080;
 const endpoint1 = '/engines';
 
-// function sendEngineList()
-// {
-//     res.status(200).send(
-//        {
-//         "id":11,
-//         "name":"enginename",
-//         "manufDate": 1984
-//        }
-//     )
-// }
+function sendEngineList(res,res)
+{
+    res.status(200).send(
+        {
+            id:11,
+            name:"enginename",
+            manufDate: 1984
+        }
+    )   
+}
 
 app.listen(
     PORT,
     () => console.log(`it's alive on localhost:${PORT}`)
 )
 
-// app.get(`$endpoint1`, (req,res)=>sendEngineList() )
-
-app.get('/engines', (req,res) => {
-    res.status(200).send(
-        {
-        id:11,
-        name:"enginename",
-        manufDate: 1984
-        }
-    )   
-});
+app.get( 
+    '/engines', 
+    (req,res) => sendEngineList(req,res) 
+);
